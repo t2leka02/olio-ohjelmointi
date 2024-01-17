15744e2802ce59575f7676e3ff4761abb7c87bf0
t2leka02 <t2leka02@students.oamk.fi>
2024-01-17 22:17:33 +0200

    teht1 vastattu

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
-
+#include <ctime>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+
+int game(int maxnum){
+
+
+    int player_guess=0;
+    int tries=0;
+    srand(time(NULL));
+    int satluku = rand() % maxnum;
+
+
+
+    do{
+
+       cout << "Arvaa luku" << endl;
+       cin>>player_guess;
+       tries++;
+
+
+       if (player_guess>satluku){
+           cout << "Luku on pienempi" << endl;
+        }
+
+
+       else if(player_guess<satluku){
+           cout << "Luku on suurempi" << endl;
+        }
+
+
+       else if(player_guess == satluku){
+           cout << "Oikea vastaus! Sait oikean luvun " << tries << " yrityksella" << endl;
+       }
+
+    } while (player_guess != satluku);
+    return tries;
+
+}
+
+
+int main() {
+int maxnum;
+cout << "anna arvattavien numeroiden maara" << endl;
+cin >> maxnum;
+game(maxnum);
+
     return 0;
 }
